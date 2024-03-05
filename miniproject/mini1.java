abf62bad37e80ca691a46e8bd1a2998262ff79d4
LOCA11 <112869504+LOCA11@users.noreply.github.com>
2024-03-06 08:19:18 +0900

    Update mini1.java

--- miniproject/mini1.java
+++ miniproject/mini1.java
@@ -3,11 +3,11 @@
     과제1
     콘솔 화면에 구구단 출력하기
 */
-public class mini1 {
-    public static void main(String[] args) {
+공개 클래스 mini1 {
+    공용 정적 보이드 메인(String[] args) {
         System.out.println("구구단 출력");
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
+        (inti = 1; i <= 9; i++) {
+ (int j = 1; j <= 9; j++) {
                 System.out.printf("%02d x %02d = %02d%n", i, j, i * j);
             }
         }
